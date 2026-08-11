d7afc571205624750ddf7a7148316b88ae465ec7
jaicova9119-source <jaicova9119@gmail.com>
2026-08-11 17:38:19 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * cargue sin señal.
  */
 
-const CACHE_NAME = "kronos-m-v32";
+const CACHE_NAME = "kronos-m-v33";
 
 /* Archivos propios. Estos tienen que quedar guardados si o si: sin ellos
    la app no abre sin señal. */
